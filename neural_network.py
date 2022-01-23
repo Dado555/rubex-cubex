79b65b397611cbd03f6ed2b9b8792cf92e2ad89b
Dado555 <dmalic555@gmail.com>
2022-01-23 19:56:54 +0100

    training

--- neural_network.py
+++ neural_network.py
@@ -11,9 +11,12 @@ from tensorflow._api.v2 import data
 def create_NN():
     model = Sequential()  # [Input(units=288, input_shape=[288], batch_size=1)]
     model.add(Dense(units=512, input_shape=(288,), activation="relu"))
+    model.add(BatchNormalization())
     # model.add(Dense(512, activation="relu"))
     model.add(Dense(256, activation="relu"))
+    model.add(BatchNormalization())
     model.add(Dense(128, activation="relu"))
+    model.add(BatchNormalization())
     model.add(Dense(1, activation="linear"))
 
     model.summary()
